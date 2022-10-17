31a9707a6847592fe173940cafce84a992a4bb21
NyatiZw <appsmaid19@gmail.com>
2022-10-17 15:27:51 +0200

    Updates

---
+++ precache-manifest.163fba36405001c20f4ab2cb55b2d64f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa31df99eb443321cde1f47df7c148b",
+    "url": "/LLS/index.html"
+  },
+  {
+    "revision": "c08a4b73f028366973d0",
+    "url": "/LLS/static/css/main.dba64202.chunk.css"
+  },
+  {
+    "revision": "cf2668857adcb44344bf",
+    "url": "/LLS/static/js/2.9e690c8b.chunk.js"
+  },
+  {
+    "revision": "c08a4b73f028366973d0",
+    "url": "/LLS/static/js/main.38ab222d.chunk.js"
+  },
+  {
+    "revision": "9e79b26d52ef5419404b",
+    "url": "/LLS/static/js/runtime-main.814275fe.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/LLS/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "c3d75740c5c58e55c4e6e9bac7e742ac",
+    "url": "/LLS/static/media/arrow-down.c3d75740.svg"
+  },
+  {
+    "revision": "d68680365ee5076d82ae3f0977fd7563",
+    "url": "/LLS/static/media/proj1.d6868036.jpg"
+  },
+  {
+    "revision": "12207fa0294e51d01bb1c92f25104fe5",
+    "url": "/LLS/static/media/proj2.12207fa0.jpg"
+  }
+]);
